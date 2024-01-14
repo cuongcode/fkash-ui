f8aa432ae7c3d1d023a598173a4507a69d32b253
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-14 14:32:21 +0700

    add font family, size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,23 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter']
+      },
+      fontSize: {
+        h1: '58px',
+        h2: '48px',
+        h3: '40px',
+        h4: '32px',
+        h5: '24px',
+        p1: '20px',
+        p2: '18px',
+        p3: '16px',
+        p4: '14px',
+        p5: '12px',
+        p6: '10px',
+      }
+    },
   },
   plugins: [],
 }
