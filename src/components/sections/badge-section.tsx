8b391874d14fdc0e82bab428e89a54df5a810490
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-15 02:40:07 +0700

    refactor badge section

--- src/components/sections/badge-section.tsx
+++ src/components/sections/badge-section.tsx
@@ -4,31 +4,11 @@ import { Badge } from '../base'
 export const BadgeSection = () => {
     return (
         <div className='flex gap-5'>
-            <div className='flex flex-col gap-2'>
-                <div className='flex gap-5'>
-                    <Badge preset='primary' text='BRAND'/>
-                </div>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <div className='flex gap-5'>
-                    <Badge preset='secondary' text='NEUTRAL'/>
-                </div>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <div className='flex gap-5'>
-                    <Badge preset='success' text='SUCCESS'/>
-                </div>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <div className='flex gap-5'>
-                    <Badge preset='warning' text='WARNING'/>
-                </div>
-            </div>
-            <div className='flex flex-col gap-2'>
-                <div className='flex gap-5'>
-                    <Badge preset='error' text='ERROR'/>
-                </div>
-            </div>
+            <Badge preset='primary' text='BRAND'/>
+            <Badge preset='secondary' text='NEUTRAL'/>
+            <Badge preset='success' text='SUCCESS'/>
+            <Badge preset='warning' text='WARNING'/>
+            <Badge preset='error' text='ERROR'/>
         </div>
     );
 };
