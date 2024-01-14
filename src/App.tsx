33c8bf594f5bccb3038844e657a3b17a1cab4640
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-15 04:05:04 +0700

    fix style generic

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,23 @@ function App() {
     <div className='flex flex-col gap-10 px-10 font-inter bg-background-black text-white'>
       <Header />
       <div className='flex flex-col gap-10 w-full max-w-screen-md m-auto'>
-        <Section name='Typography'>
-          <Typography />
-        </Section>
-        <Section name='Color'>
-          <Color />
-        </Section>
         <Section name='Button'>
           <ButtonSection />
-        </Section>
-        <Section name='Badge'>
-          <BadgeSection />
         </Section>
         <Section name='Checkbox'>
           <CheckboxSection />
         </Section>
+        <Section name='Badge'>
+          <BadgeSection />
+        </Section>
         <Section name='Notification'>
           <NotificationSection />
+        </Section>
+        <Section name='Color'>
+          <Color />
+        </Section>
+        <Section name='Typography'>
+          <Typography />
         </Section>
       </div>
       <Footer />
--- src/components/base/header.tsx
+++ src/components/base/header.tsx
@@ -4,7 +4,7 @@ import { Tab } from './tab'
 
 export const Header = () => {
     return (
-      <div className='flex border-b border-gray-600 items-center'>
+      <div className='flex border-b border-gray-600 items-center justify-center'>
         <Text preset='p1' text='Fkash UI' className='border-b-4 border-transparent text-primary-100 font-bold mr-4'/>
         <Tab preset='header' text='Portfolio'/>
         <Tab preset='header' text='Dashboard'/>
@@ -12,7 +12,7 @@ export const Header = () => {
         <Tab preset='header' text='Currencies'/>
         <Tab preset='header' text='Career'/>
         <Tab preset='header' text='About'/>
-        <Button preset='primary' text='Sign In' className='w-32 ml-4 hidden lg:block'/>
+        <Button preset='primary' text='Sign In' className='w-28 ml-4 hidden lg:block'/>
       </div>
     );
   };
